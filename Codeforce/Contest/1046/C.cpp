30d186f013dabd6d234861e88a9a21ed4c7914d5
Nguyễn Phú Hùng <hung.phunguyen8888@gmail.com>
2025-08-31 17:21:23 +0700

    solve problem 2136C using dp and queue

--- Codeforce/Contest/1046/C.cpp
+++ Codeforce/Contest/1046/C.cpp
@@ -29,6 +29,28 @@ bool multiple_testcases = true;
 
 void solve() 
 {
+	int n;
+	cin >> n;
+	vector<int> a(n + 1);
+	for(int i = 1; i <= n; i++)
+		cin >> a[i];
+
+	vector<int> dp(n + 1, 0);
+	vector<deque<int>> dq(n + 1);
+
+	for(int i = 1; i <= n; i++)
+	{
+		dp[i] = dp[i - 1];
+
+		dq[a[i]].push_back(i);
+		if(dq[a[i]].size() == a[i])
+		{
+			dp[i] = max(dp[i], dp[dq[a[i]].front() - 1] + a[i]);
+			dq[a[i]].pop_front();
+		}
+	}
+	
+	cout << dp[n] << '\n';
 
 }
 int main() 
